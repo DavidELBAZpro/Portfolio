0d8b29e9cb54e1275f2eeab26ceba54e7cc7b0d7
Dvd Elbaz <david.elbaz.pro@gmail.com>
2023-01-17 15:26:11 +0100

    Portfolio data set

--- src/data/portfolio.data.js
+++ src/data/portfolio.data.js
@@ -5,29 +5,30 @@ export const portfolioData = [
     {
         id: 1,
         image: MelodifyImg,
-        title: 'Hello',
-        githubUrl: 'https://github.com',
-        demoUrl: 'https://google.com'
+        title: "Melodify",
+        githubUrl: "https://github.com/Aviv-rs/Melodify",
+        demoUrl: "https://melodify.onrender.com/music"
     },
     {
         id: 2,
         image: MelodifyImg,
-        title: 'Melodify',
-        githubUrl: 'https://github.com',
-        demoUrl: 'https://google.com'
+        title: "PAN - Physicians Association for Nutrition",
+        githubUrl: "https://github.com/Dalya1812/panProject",
+        demoUrl: "https://pan-int.org"
     },
     {
         id: 3,
         image: MelodifyImg,
-        title: 'Melodify',
-        githubUrl: 'https://github.com',
-        demoUrl: 'https://google.com'
+        title: "BnB ERP",
+        githubUrl: "https://github.com/DavidELBAZpro",
+        demoUrl: "https://www.google.com/search?q=this+project+is+private&rlz=1C5CHFA_enIL914IL914&oq=this+project+is+private&aqs=chrome..69i57j0i22i30j0i390l2.5884j0j7&sourceid=chrome&ie=UTF-8"
     },
     {
         id: 4,
         image: MelodifyImg,
-        title: 'Melodify',
-        githubUrl: 'https://github.com',
-        demoUrl: 'https://google.com'
+        title: "Melodify",
+        githubUrl: "https://github.com/Aviv-rs/Melodify",
+        demoUrl: "https://melodify.onrender.com/music"
     },
+   
 ]
