cd8d7337d49f3c8f44a4cb8437d5427d5c7d8615
Dvd Elbaz <david.elbaz.pro@gmail.com>
2023-02-20 12:49:11 +0100

    Update of CV import

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import { Header } from "./cmps/header"
-import { Navbar } from "./cmps/navbar"
-import { Experiences } from "./cmps/experiences"
-import { Services } from "./cmps/services"
-import { Portfolio } from "./cmps/portfolio"
-import { Testimonials } from "./cmps/testimonials"
-import { Contact } from "./cmps/contact"
-import { Footer } from "./cmps/footer"
-import { About } from "./cmps/about"
+import { Header } from './cmps/header'
+import { Navbar } from './cmps/navbar'
+import { Experiences } from './cmps/experiences'
+import { Services } from './cmps/services'
+import { Portfolio } from './cmps/portfolio'
+import { Testimonials } from './cmps/testimonials'
+import { Contact } from './cmps/contact'
+import { Footer } from './cmps/footer'
+import { About } from './cmps/about'
 
 export function App() {
   return (
--- src/cmps/DynamicCmps/cta.jsx
+++ src/cmps/DynamicCmps/cta.jsx
@@ -1,10 +1,21 @@
-import CV from '/Users/dvdelbaz/Desktop/portfolioWebsite/davidElbaz_portfolio/src/assets/imgs/CV-DAVID-ELBAZ.pdf'
+import CV from '../../assets/imgs/CV-DAVID-ELBAZ.pdf'
 
 export const CTA = () => {
-    return (
-        <div className="cta">
-            <a href={CV} className="btn" download>Download CV</a>
-            <a href="#contact" className="btn btn-primary">Contact</a>
-        </div>
-    )
+  return (
+    <div className="cta">
+      <a
+        href={CV}
+        className="btn"
+        download
+      >
+        Download CV
+      </a>
+      <a
+        href="#contact"
+        className="btn btn-primary"
+      >
+        Contact
+      </a>
+    </div>
+  )
 }
