f7c9794efb8f53ed9652b06ff497f039755292ef
Dvd Elbaz <david.elbaz.pro@gmail.com>
2023-02-22 10:04:31 +0100

    Update CV download link

--- src/cmps/DynamicCmps/cta.jsx
+++ src/cmps/DynamicCmps/cta.jsx
@@ -1,11 +1,10 @@
-import CV from '../../assets/imgs/CV-DAVID-ELBAZ.pdf'
-
 export const CTA = () => {
   return (
     <div className="cta">
       <a
-        href={CV}
+        href="https://share.internxt.com/d/sh/file/a428c800d5cc22554aac/21ff8f14223f28be1f245a2915819be2436cfa0adf65a80e9e4ca668a57ee289"
         className="btn"
+        target="_blank"
         download
       >
         Download CV
