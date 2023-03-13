31dfcb0b214a0593d429558d3ce045b4df5d3144
DavidElbazPro <david.elbaz.pro@gmail.com>
2023-03-13 14:06:05 +0100

    Added CNAME file

--- src/cmps/DynamicCmps/cta.jsx
+++ src/cmps/DynamicCmps/cta.jsx
@@ -2,17 +2,15 @@ export const CTA = () => {
   return (
     <div className="cta">
       <a
-        href="https://share.internxt.com/d/sh/file/a428c800d5cc22554aac/21ff8f14223f28be1f245a2915819be2436cfa0adf65a80e9e4ca668a57ee289"
+        // href="https://share.internxt.com/d/sh/file/a428c800d5cc22554aac/21ff8f14223f28be1f245a2915819be2436cfa0adf65a80e9e4ca668a57ee289"
         className="btn"
+        href="https://drive.google.com/drive/folders/1jzSB7KUMI8UYH4HnNNAULuffejEjZr5B?usp=sharing"
         target="_blank"
         download
       >
         Download CV
       </a>
-      <a
-        href="#contact"
-        className="btn btn-primary"
-      >
+      <a href="#contact" className="btn btn-primary">
         Contact
       </a>
     </div>
