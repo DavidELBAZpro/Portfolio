b9e1e7ee9cdc66a2182e1a361666e5e8cd2f65d8
DavidElbazPro <david.elbaz.pro@gmail.com>
2023-03-13 14:30:29 +0100

    cta link changed

--- src/cmps/DynamicCmps/cta.jsx
+++ src/cmps/DynamicCmps/cta.jsx
@@ -4,7 +4,7 @@ export const CTA = () => {
       <a
         // href="https://share.internxt.com/d/sh/file/a428c800d5cc22554aac/21ff8f14223f28be1f245a2915819be2436cfa0adf65a80e9e4ca668a57ee289"
         className="btn"
-        href="https://drive.google.com/drive/folders/1jzSB7KUMI8UYH4HnNNAULuffejEjZr5B?usp=sharing"
+        href="https://drive.google.com/uc?export=download&id=18u0eaBAd7InuUnB0uA3kFROU2kEt3Gc0"
         target="_blank"
         download
       >
