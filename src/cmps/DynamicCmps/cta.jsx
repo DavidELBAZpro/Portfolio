ff1753a4aa9210c5e9d720005196aa3a5f327f99
Dvd Elbaz <dvdelbaz@mbp-de-dvd.home>
2023-04-03 16:52:50 +0200

    new link to CV

--- src/cmps/DynamicCmps/cta.jsx
+++ src/cmps/DynamicCmps/cta.jsx
@@ -4,7 +4,7 @@ export const CTA = () => {
       <a
         // href="https://share.internxt.com/d/sh/file/a428c800d5cc22554aac/21ff8f14223f28be1f245a2915819be2436cfa0adf65a80e9e4ca668a57ee289"
         className="btn"
-        href="https://drive.google.com/uc?export=download&id=18u0eaBAd7InuUnB0uA3kFROU2kEt3Gc0"
+        href="https://drive.google.com/uc?export=download&id=1vECiSLz0GvCH_XLmNTFM4MSIm2WIZV6J"
         target="_blank"
         download
       >
